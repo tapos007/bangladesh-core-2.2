5618a46a2002caf933547f797ed9f3ef645748cc
tapos <tapos.aa@gmail.com>
2019-03-05 01:54:36 +0600

    initial work done

---
+++ Application.API/Controllers/ProductController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Application.API.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Application.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        // GET: api/Product
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(ProductHelper.Get());
+        }
+
+        // GET: api/Product/5
+        [HttpGet("{id}", Name = "Get")]
+        public IActionResult Get(int id)
+        {
+            return Ok(ProductHelper.GetAProduct(id));
+        }
+
+        // POST: api/Product
+        [HttpPost]
+        public IActionResult Post([FromBody] Product product)
+        {
+            return Ok(ProductHelper.AddProduct(product));
+        }
+
+        // PUT: api/Product/5
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody] Product product)
+        {
+            return Ok(ProductHelper.Update(id,product));
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            return Ok(ProductHelper.DeleteProduct(id));
+        }
+    }
+}
---
+++ Application.API/Models/ProductHelper.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Application.API.Models
+{
+    public static class ProductHelper
+    {
+        private static List<Product> Products = new List<Product>()
+        {
+            new Product()
+            {
+                Id = 1,
+                Name = "java how to program",
+                Description = "its a good book"
+            },
+            new Product()
+            {
+                Id = 1,
+                Name = "C# best book",
+                Description = "its a good book"
+            },
+        };
+
+        public static Product AddProduct(Product product)
+        {
+            product.Id = Products.Count + 1;
+            Products.Add(product);
+            return product;
+        }
+
+        public static Product GetAProduct(int id)
+        {
+            return Products.FirstOrDefault(x => x.Id == id);
+        }
+
+        public static List<Product> Get()
+        {
+            return Products;
+        }
+
+        public static Product Update(int id, Product product)
+        {
+            var findProduct = Products.FirstOrDefault(x => x.Id == id);
+            findProduct.Name = product.Name;
+            findProduct.Description = product.Description;
+            return findProduct;
+        }
+
+        public static Product DeleteProduct(int id)
+        {
+            var product = Products.FirstOrDefault(x => x.Id == id);
+            var products = Products.Where(x => x.Id != id).ToList();
+            Products = products;
+            return product;
+
+        }
+    }
+}
